16b9f6116c45ec8b3a2808936ed6be6e1a112381
Mad Dinh <phong.dinh2108@gmail.com>
2026-08-13 14:58:20 +0700

    docs(cache): add compression native oracle

--- example/app.config.js
+++ example/app.config.js
@@ -4,20 +4,22 @@ module.exports = () => {
   const config = staticConfig.expo;
   const testSalt = process.env.EAS_LOCAL_CACHE_TEST_SALT;
   const environmentKey = process.env.EAS_LOCAL_CACHE_TEST_ENVIRONMENT_KEY;
+  const compression = process.env.EAS_LOCAL_CACHE_TEST_COMPRESSION;
 
-  if (!testSalt && !environmentKey) {
+  if (!testSalt && !environmentKey && !compression) {
     return config;
   }
 
   return {
     ...config,
-    ...(environmentKey
+    ...(environmentKey || compression
       ? {
           buildCacheProvider: {
             ...config.buildCacheProvider,
             options: {
               ...config.buildCacheProvider.options,
-              environmentKey,
+              ...(environmentKey ? { environmentKey } : {}),
+              ...(compression ? { compression } : {}),
             },
           },
         }
